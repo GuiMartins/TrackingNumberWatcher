1f604d7f79b0c827faca1f890c1bf9888414bf85
GuiMartins <guilherme_cjs@outlook.com>
2014-09-30 17:34:26 -0300

    att func added

--- src/trackingnumberwatcher/TrackingNumberWatcherView.java
+++ src/trackingnumberwatcher/TrackingNumberWatcherView.java
@@ -2,6 +2,9 @@ package trackingnumberwatcher;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jdesktop.application.Action;
@@ -19,14 +22,30 @@ public class TrackingNumberWatcherView extends FrameView {
     private TrackingNumberWatcherDetailsFrame detailsFrame;
     private JDialog addFrame, aboutBox;
     private String lastDate, codValue;
+    private int attRate = 1; //1h
+    private Timer timer = new Timer();
     
     public TrackingNumberWatcherView(SingleFrameApplication app) {
         super(app);
         initComponents();
         dbConn.initiateDB();
         loadSavedData();
+        attCheck();
     }
 
+    private void attCheck()
+    {
+        timer.schedule( new TimerTask()
+        {
+            public void run()
+            {
+                System.out.println("------------------------------------\n" + new Date());
+                System.out.println("atualizando a cada " + attRate + " hora(s)");
+                checkMovementation();
+            }
+        }, 0, attRate*360000);
+    }
+    
     @Action
     public void showAboutBox() {
         if (aboutBox == null) {
@@ -68,11 +87,15 @@ public class TrackingNumberWatcherView extends FrameView {
         contents = new javax.swing.JPanel();
         tableScrollPane = new javax.swing.JScrollPane();
         mainTable = new javax.swing.JTable();
+        buttonsPanel = new javax.swing.JPanel();
         adicionarButton = new javax.swing.JButton();
         detalhesButton = new javax.swing.JButton();
         removerButton = new javax.swing.JButton();
+        atualizarLabel = new javax.swing.JLabel();
+        atualizarCombo = new javax.swing.JComboBox();
         esconderButton = new javax.swing.JButton();
         sairButton = new javax.swing.JButton();
+        atualizarButton = new javax.swing.JButton();
         menuBar = new javax.swing.JMenuBar();
         javax.swing.JMenu fileMenu = new javax.swing.JMenu();
         javax.swing.JMenuItem exitMenuItem = new javax.swing.JMenuItem();
@@ -117,6 +140,9 @@ public class TrackingNumberWatcherView extends FrameView {
         mainTable.getColumnModel().getColumn(1).setHeaderValue(resourceMap.getString("mainTable.columnModel.title1")); // NOI18N
         mainTable.getColumnModel().getColumn(2).setHeaderValue(resourceMap.getString("mainTable.columnModel.title2")); // NOI18N
 
+        buttonsPanel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        buttonsPanel.setName("buttonsPanel"); // NOI18N
+
         adicionarButton.setText(resourceMap.getString("adicionarButton.text")); // NOI18N
         adicionarButton.setName("adicionarButton"); // NOI18N
         adicionarButton.addActionListener(new java.awt.event.ActionListener() {
@@ -141,6 +167,19 @@ public class TrackingNumberWatcherView extends FrameView {
             }
         });
 
+        atualizarLabel.setFont(new java.awt.Font("Dialog", 1, 10)); // NOI18N
+        atualizarLabel.setText(resourceMap.getString("atualizarLabel.text")); // NOI18N
+        atualizarLabel.setToolTipText(resourceMap.getString("atualizarLabel.toolTipText")); // NOI18N
+        atualizarLabel.setName("atualizarLabel"); // NOI18N
+
+        atualizarCombo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1 hora", "2 horas", "3 horas", "4 horas", "5 horas", "6 horas" }));
+        atualizarCombo.setName("atualizarCombo"); // NOI18N
+        atualizarCombo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                atualizarComboActionPerformed(evt);
+            }
+        });
+
         esconderButton.setText(resourceMap.getString("esconderButton.text")); // NOI18N
         esconderButton.setName("esconderButton"); // NOI18N
         esconderButton.addActionListener(new java.awt.event.ActionListener() {
@@ -158,38 +197,71 @@ public class TrackingNumberWatcherView extends FrameView {
             }
         });
 
+        atualizarButton.setText(resourceMap.getString("atualizarButton.text")); // NOI18N
+        atualizarButton.setName("atualizarButton"); // NOI18N
+        atualizarButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                atualizarButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout buttonsPanelLayout = new javax.swing.GroupLayout(buttonsPanel);
+        buttonsPanel.setLayout(buttonsPanelLayout);
+        buttonsPanelLayout.setHorizontalGroup(
+            buttonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(buttonsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(buttonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(adicionarButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                    .addComponent(sairButton, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                    .addComponent(esconderButton, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                    .addComponent(detalhesButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                    .addComponent(removerButton, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                    .addComponent(atualizarLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                    .addComponent(atualizarCombo, javax.swing.GroupLayout.Alignment.TRAILING, 0, 100, Short.MAX_VALUE)
+                    .addComponent(atualizarButton, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        buttonsPanelLayout.setVerticalGroup(
+            buttonsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(buttonsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(adicionarButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(detalhesButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(removerButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(atualizarLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(atualizarCombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(atualizarButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+                .addComponent(esconderButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(sairButton)
+                .addContainerGap())
+        );
+
         javax.swing.GroupLayout contentsLayout = new javax.swing.GroupLayout(contents);
         contents.setLayout(contentsLayout);
         contentsLayout.setHorizontalGroup(
             contentsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, contentsLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(tableScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
-                .addGroup(contentsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(adicionarButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(detalhesButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(removerButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(esconderButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(sairButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(tableScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 501, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(buttonsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         contentsLayout.setVerticalGroup(
             contentsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(contentsLayout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, contentsLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(contentsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(tableScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 226, Short.MAX_VALUE)
-                    .addGroup(contentsLayout.createSequentialGroup()
-                        .addComponent(adicionarButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(detalhesButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(removerButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 65, Short.MAX_VALUE)
-                        .addComponent(esconderButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(sairButton)))
+                .addGroup(contentsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(tableScrollPane, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 338, Short.MAX_VALUE)
+                    .addComponent(buttonsPanel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -263,7 +335,7 @@ public class TrackingNumberWatcherView extends FrameView {
     }//GEN-LAST:event_detalhesButtonActionPerformed
 
     private void sairButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sairButtonActionPerformed
-        System.exit(1);
+        System.exit(0);
     }//GEN-LAST:event_sairButtonActionPerformed
 
     private void removerButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_removerButtonActionPerformed
@@ -275,10 +347,21 @@ public class TrackingNumberWatcherView extends FrameView {
     }//GEN-LAST:event_removerButtonActionPerformed
     
     private void esconderButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_esconderButtonActionPerformed
-        checkAtt();
+        
     }//GEN-LAST:event_esconderButtonActionPerformed
 
-    private void checkAtt()
+    private void atualizarComboActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_atualizarComboActionPerformed
+        timer.cancel();
+        timer = new Timer();
+        attRate = atualizarCombo.getSelectedIndex() + 1;
+        attCheck();
+    }//GEN-LAST:event_atualizarComboActionPerformed
+
+    private void atualizarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_atualizarButtonActionPerformed
+        checkMovementation();
+    }//GEN-LAST:event_atualizarButtonActionPerformed
+
+    private void checkMovementation()
     {
         out("Checkando atualização");
         JSON json = new JSON();
@@ -314,6 +397,10 @@ public class TrackingNumberWatcherView extends FrameView {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton adicionarButton;
+    private javax.swing.JButton atualizarButton;
+    private javax.swing.JComboBox atualizarCombo;
+    private javax.swing.JLabel atualizarLabel;
+    private javax.swing.JPanel buttonsPanel;
     private javax.swing.JPanel contents;
     private javax.swing.JButton detalhesButton;
     private javax.swing.JButton esconderButton;
